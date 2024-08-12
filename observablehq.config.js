4faed0e6b997e5ead0354a37605c46c1bd3948e2
Ernesto Laval <ernesto.laval@gmail.com>
2024-08-12 18:06:11 -0400

    Update config & cleanup

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,21 +17,27 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
-
+  head: `
+  <link rel="icon" href="favicon.ico" type="image/png" sizes="32x32">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <!--
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  -->
+  `,
   // The path to the source root.
   root: "src",
 
-  // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
-  sidebar: false, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
-  // output: "dist", // path to the output root for build
-  // search: true, // activate search
-  // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // cleanUrls: true, // drop .html from URLs
-};
+   // Some additional configuration options and their defaults:
+   theme: "default", // try "light", "dark", "slate", etc.
+   header: 'Un relato con datos <img src="./favicon.ico" height="20px"> <a href="https://www.relatoscondatos.cl" target="_blank">www.relatoscondatos.cl</a> ', // what to show in the header (HTML)
+   footer: 'Relatos con datos <i class="fas fa-envelope"></i> <a href="mailto:contacto@relatoscondatos.cl">contacto@relatoscondatos.cl</a>', // what to show in the footer (HTML)
+   sidebar: false, // whether to show the sidebar
+   toc: {show:false, label:"Contenido"}, // whether to show the table of contents
+   pager: false, // whether to show previous & next links in the footer
+   // output: "dist", // path to the output root for build
+   // search: true, // activate search
+   // linkify: true, // convert URLs in Markdown to links
+   typographer: true, // smart quotes and other typographic improvements
+   // cleanUrls: true, // drop .html from URLs
+ };
